9d925ffa23e68c7bc4bdd9950a7bd5a00e7d5b06
Rohit Dutta <rohitdutta2212rd@gmail.com>
2025-09-18 08:57:31 +0530

    Added Forgot password with otp funciton

--- src/main/java/com/salesSavvy/repository/UsersRepository.java
+++ src/main/java/com/salesSavvy/repository/UsersRepository.java
@@ -7,4 +7,6 @@ import com.salesSavvy.entity.Users;
 public interface UsersRepository extends JpaRepository<Users, Long> {
 	Users findByUsername(String username);
 	Users findByPassword(String password);
+	// In UsersRepository.java
+	Users findByEmail(String email);
 }
